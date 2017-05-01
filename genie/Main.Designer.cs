781290feea0f141461418025b9a99828b71139fd
rfire <rfirechou@gmail.com>
2017-05-01 19:30:06 +0800

    1. modify record format 2. add read function

--- genie/Main.Designer.cs
+++ genie/Main.Designer.cs
@@ -30,8 +30,9 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.read = new System.Windows.Forms.Button();
+            this.readStat = new System.Windows.Forms.Button();
             this.save = new System.Windows.Forms.Button();
+            this.read = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -46,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(665, 128);
+            this.button2.Location = new System.Drawing.Point(665, 113);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(84, 28);
             this.button2.TabIndex = 1;
@@ -54,19 +55,19 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // read
+            // readStat
             // 
-            this.read.Location = new System.Drawing.Point(665, 204);
-            this.read.Name = "read";
-            this.read.Size = new System.Drawing.Size(84, 28);
-            this.read.TabIndex = 2;
-            this.read.Text = "讀取資料";
-            this.read.UseVisualStyleBackColor = true;
-            this.read.Click += new System.EventHandler(this.read_stat_Click);
+            this.readStat.Location = new System.Drawing.Point(665, 412);
+            this.readStat.Name = "readStat";
+            this.readStat.Size = new System.Drawing.Size(84, 28);
+            this.readStat.TabIndex = 2;
+            this.readStat.Text = "讀取統計";
+            this.readStat.UseVisualStyleBackColor = true;
+            this.readStat.Click += new System.EventHandler(this.read_stat_Click);
             // 
             // save
             // 
-            this.save.Location = new System.Drawing.Point(665, 253);
+            this.save.Location = new System.Drawing.Point(665, 220);
             this.save.Name = "save";
             this.save.Size = new System.Drawing.Size(84, 28);
             this.save.TabIndex = 3;
@@ -74,17 +75,28 @@
             this.save.UseVisualStyleBackColor = true;
             this.save.Click += new System.EventHandler(this.save_Click);
             // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(665, 186);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(84, 28);
+            this.read.TabIndex = 4;
+            this.read.Text = "讀取資料";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(793, 452);
-            this.Controls.Add(this.save);
             this.Controls.Add(this.read);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.readStat);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "Genie 代購統計程式 Beta開發版";
             this.ResumeLayout(false);
 
         }
@@ -93,8 +105,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.Button readStat;
         private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button read;
     }
 }
 
--- genie/Main.cs
+++ genie/Main.cs
@@ -17,6 +17,8 @@ namespace genie
         public product_t[] product = new product_t[500];
         public int product_count = 0;
 
+        enum ReadState { NO_STATE = 0, READ_PRODUCT, READ_CUSTOMER, READ_ORDER };
+
         public Main()
         {
             InitializeComponent();
@@ -152,7 +154,7 @@ namespace genie
 
             File.Create("record.txt").Close() ;
 
-            StreamWriter file = new StreamWriter(@"record.txt");
+            StreamWriter file = new StreamWriter(@"record.txt", false, Encoding.Default);
 
             file.WriteLine("$$$");
 
@@ -167,12 +169,29 @@ namespace genie
                 */
                 string save_info = "";
 
-                save_info += (pro_idx + ". " + product[pro_idx].name + ": ");
-                save_info += (product[pro_idx].price + ", ");
-                save_info += (product[pro_idx].cost + ", ");
+                save_info += (pro_idx + " " + product[pro_idx].name + " ");
+                save_info += (product[pro_idx].price + " ");
+                save_info += (product[pro_idx].cost + " ");
                 save_info += ((product[pro_idx].remarks == "")? "N/A" : product[pro_idx].remarks);
                 file.WriteLine(save_info);
             }
+
+            file.WriteLine("***");
+
+            file.WriteLine();
+            file.WriteLine("@@@");
+            
+            for (int cus_idx=0; cus_idx < 500; cus_idx++)
+            {
+                if (customer[cus_idx].name.Length == 0)
+                {
+                    break;
+                }
+
+                file.WriteLine(customer[cus_idx].name);
+            }
+
+            file.WriteLine("***");
 
             file.WriteLine();
             file.WriteLine("###");
@@ -202,12 +221,142 @@ namespace genie
                 file.WriteLine(save_info);
             }
 
+            file.WriteLine("***");
 
             MessageBox.Show("保存成功");
 
             file.Close();
         }
-    }
+
+        private void read_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("record.txt"))
+            {
+                MessageBox.Show("record.txt不存在!");
+                return;
+            }
+
+            StreamReader file = new StreamReader(@"record.txt", Encoding.Default);
+
+            string line;
+            ReadState state = ReadState.NO_STATE;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                switch (line)
+                {
+                    case "$$$":
+                        state = ReadState.READ_PRODUCT;
+                        break;
+                    case "@@@":
+                        state = ReadState.READ_CUSTOMER;
+                        break;
+                    case "###":
+                        state = ReadState.READ_ORDER;
+                        break;
+                    case "***":
+                        state = ReadState.NO_STATE;
+                        break;
+                    default:
+                        switch (state)
+                        {
+                            case ReadState.READ_PRODUCT:
+
+                                string[] split = line.Split(' ');
+
+                                if (product_count == 500)
+                                {
+                                    MessageBox.Show("商品爆表，要增加array上限");
+                                }
+                                else
+                                {
+                                    product[product_count].name = split[1];
+                                    product[product_count].price = Int32.Parse(split[2]);
+                                    product[product_count].cost = Int32.Parse(split[3]);
+                                    if (split[3] == "N/A")
+                                    {
+                                        product[product_count].remarks = "";
+                                    }
+                                    else
+                                    {
+                                        product[product_count].remarks = split[4];
+                                    }
+
+                                    product_count++;
+                                }
+
+                                break;
+
+                            case ReadState.READ_CUSTOMER:
+
+                                for (int cust_idx = 0; cust_idx < 500; cust_idx++)
+                                {
+                                    if (customer[cust_idx].name.Length == 0)
+                                    {
+                                        customer[cust_idx].name = line;
+                                        break;
+                                    }
+
+                                    if (cust_idx == 499)
+                                    {
+                                        MessageBox.Show("客人爆表，要增加array上限");
+                                    }
+                                }
+
+                                break;
+
+                            case ReadState.READ_ORDER:
+
+                                string[] name_and_list = line.Split(':');
+
+                                for (int cust_idx = 0; cust_idx < 500; cust_idx++)
+                                {
+                                    if (customer[cust_idx].name == name_and_list[0])
+                                    {
+                                        name_and_list[1] = name_and_list[1].Trim();
+
+                                        string[] order_list = name_and_list[1].Split(' ');
+
+                                        for (int list_idx = 0; list_idx < order_list.Length; list_idx++)
+                                        {
+                                            string[] index_and_quantity = order_list[list_idx].Split('-');
+
+                                            for (int ord_idx = 0; ord_idx < 50; ord_idx++)
+                                            {
+                                                if (customer[cust_idx].order[ord_idx].index == -1)
+                                                {
+                                                    customer[cust_idx].order[ord_idx].index = Int32.Parse(index_and_quantity[0]);
+                                                    customer[cust_idx].order[ord_idx].quantity = Int32.Parse(index_and_quantity[1]);
+                                                    break;
+                                                }
+
+                                                if (ord_idx == 49)
+                                                {
+                                                    MessageBox.Show("訂單超過上限?");
+                                                }
+                                            }
+                                        }
+
+                                        break;
+                                    }
+
+                                    if (cust_idx == 499)
+                                    {
+                                        MessageBox.Show("找不到這個訂單的人?");
+                                    }
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                        break;
+                }
+            }
+
+            MessageBox.Show("讀取完畢");
+        }
+}
 
     public class product_t
     {
